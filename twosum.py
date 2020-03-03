ace7bd28dda55c14479c8b2124bbeca450194c06
Paanda <pandapanhaoran@gmail.com>
2020-03-03 10:12:49 +0000

    Create twosum.py

---
+++ twosum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(0,len(nums)):
+            if target - nums[i] in nums:
+                yield [i,nums.index(target - nums[i])]
