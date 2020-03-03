9a8f997bcfcf6431a43ca2346812ab77523b6d36
Paanda <pandapanhaoran@gmail.com>
2020-03-03 10:11:22 +0000

    Create collections_package_.py

---
+++ collections_package_.py
@@ -0,0 +1,18 @@
+import collections
+
+
+
+# '''chain maps'''
+# baseline = {'music': 'bach', 'art': 'rembrandt'}
+# adjustments = {'art': 'van gogh', 'opera': 'carmen'}
+# updated = collections.ChainMap(baseline,adjustments)
+# print(updated.new_child())
+# print(updated.parents)
+#
+
+'''Counter'''
+c = collections.Counter(cats=4, dogs=8, man = 12)
+# print(sorted(c.elements()))
+# print(c.most_common(3))
+# d = collections.Counter({'cats':2,'dogs':2})
+# c.subtract(d)
