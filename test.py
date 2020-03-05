4315f76b49b2e72b06fb8c599f06aced844b6100
Paandahaoran <44772543+Paandahaoran@users.noreply.github.com>
2020-03-06 04:12:16 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 import numpy as np
-import re
-i = np.arange(1,16,1)
-i = sorted(i)
-i = str(i).replace(']',', ').replace('[',', ').split(', ')
-i = [x for x in i if x != '']
-print(sorted(i))
+
+lam = np.random.beta(100,100)
+
+print(lam)
